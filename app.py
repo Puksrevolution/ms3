bd013119fa855ff060b7782ba43b8bc52405a10b
Puksrevolution <puksbusinessworld@gmail.com>
2021-07-13 01:15:00 +0000

    commit

--- app.py
+++ app.py
@@ -23,6 +23,16 @@ def index():
     return render_template("index.html", page_title="Yummy Recipes")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html", page_title="About Us")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact Us")
+
+
 @app.route("/accessibility")
 def accessibility():
     return render_template("accessibility.html", page_title="Accessibility")
@@ -38,34 +48,84 @@ def privacy():
     return render_template("privacy.html", page_title="Privacy Policy")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html", page_title="About Us")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html", page_title="Register")
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", page_title="Contact Us")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html", page_title="Log In")
 
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html", page_title="Profile")
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template(
+            "profile.html", username=username, page_title="Profile")
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 @app.route("/add_recipe")
 def addRecipe():
     return render_template("add_recipe.html", page_title="Add Recipe")
-
-
-@app.route("/register")
-def register():
-    return render_template("register.html", page_title="Register")
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html", page_title="Log In")
 
 
 @app.route("/favorite")
@@ -75,8 +135,9 @@ def favorite():
 
 @app.route("/favorites")
 def favorites():
+    favorites = mongo.db.favorites.find()
     return render_template(
-        "favorites.html", page_title="My Favorites")
+        "favorites.html", favorites=favorites, page_title="My Favorites")
 
 
 @app.route("/recipe")
@@ -89,6 +150,18 @@ def recipes():
     recipes = mongo.db.recipes.find()
     return render_template(
         "recipes.html", recipes=recipes, page_title="Recipes")
+
+
+# ---- NEWSLETTER SUBSCRIPTION FORM ----- #
+
+
+@ app.route('/sub', methods=['POST'])
+def sub():
+    sub = mongo.db.newsletter
+    return_data = request.form.to_dict()
+    sub.insert_one(return_data)
+    flash("Sucessfully Subscribed")
+    return redirect(request.referrer)
 
 
 if __name__ == "__main__":
